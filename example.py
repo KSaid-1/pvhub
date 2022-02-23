898fab7de594f8f4eccc656be30d844f989cf416
Khaled Said <khaled.s.sulaiman@gmail.com>
2022-02-23 21:58:58 +1000

    Anthony's contributions

--- example.py
+++ example.py
@@ -1,15 +1,11 @@
-from pvhub import*
-#-------------------------------------------------------------------------------------#
-f0 = open("./inputs/example.csv","r")
-#--------------------------------------------------------------------------------------#
-for line in f0:
-    if line[0]!='#':
-        SNID = str(line.split(",")[0])
-        folder01 = str(line.split(",")[1])
-        ra00 = float(line.split(",")[5])
-        dec00 = float(line.split(",")[6])
-        z00 = float(line.split(",")[9])
-        pv = calculate_pv(SNID,folder01,ra00,dec00,z00,extrapolation="Yes")
-        print (SNID,pv)
-f0.close()
-#---------------------------------------------------------------------------------------#
+from pvhub_new import *
+import pandas as pd
+
+inp = pd.read_csv("./inputs/example.csv")
+SNID = inp["SNID"]
+folder01 = inp["folder"]
+ra00 = inp["RA_host"]
+dec00 = inp["Dec_host"]
+z00 = inp["zcmb"]
+pv = calculate_pv(ra00, dec00, z00, extrapolation=False)
+print(pv)
